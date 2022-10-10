7394bc3e25f8a9a60f15cb795b865914227f8be6
Asad Ullah Rafi <93393646+asadation@users.noreply.github.com>
2022-10-10 10:33:37 +0500

    Add files via upload

---
+++ pre-and-user.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+useradd -s /bin/bash -d /opt/stack -m stack
+sudo chmod +x /opt/stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
+mkdir ~/.ssh; chmod 700 ~/.ssh
